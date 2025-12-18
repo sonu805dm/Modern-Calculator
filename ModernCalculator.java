72dea95dfdf90bbdb420f3409ab8e7218d19d47f
sonu805dm <sonu805dm@gmail.com>
2025-12-18 16:33:11 +0530

    the equation is taken as user input and it is filtered to suitable for the algorithm.

--- ModernCalculator.java
+++ ModernCalculator.java
@@ -3,6 +3,14 @@ import java.util.StringTokenizer;
 
 public class ModernCalculator {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the equation (the equation must be mathematically correct and must end with '=' sign): ");
+        String equation = sc.nextLine();
+
+        // filtering the equation
+        equation = equation.replace(" ", "");
+        equation = equation.replace("=", "");
 
     }
 }
